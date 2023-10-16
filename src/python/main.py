33451242a34080c0be68104cd7aa46a05d244fb4
yanguojin <guojin_yjs@cumt.edu.cn>
2023-10-16 10:03:35 +0800

    Differentiate and implement different model cases

--- src/python/main.py
+++ src/python/main.py
@@ -10,25 +10,25 @@
 from process import print_info
 from openvino_deploy_rtdetr import rtdert_infer
 
-def main():
+def main(postprocess = True):
     """
     The main function prints a greeting message, sets the paths for a model, an image, and a label file,
     and then calls the rtdert_infer function with these paths and some additional parameters.
     """
     print_info("This is an RT-DETR model deployment case using Python!")
-    
-    # model_path = "E:\\Model\\rtdetr_r50vd_6x_coco.onnx"
-    # image_path = "E:\\GitSpace\\RT-DETR-OpenVINO\\image\\000000570688.jpg"
-    # lable_path = "E:\\GitSpace\\OpenVINO-CSharp-API\dataset\\lable\\COCO_lable.txt"
-    # rtdert_infer(model_path, image_path, "CPU", lable_path,True)
-
-    model_path = "E:\\Model\\RT-DETR\\rtdetr_r50vd_6x_coco.xml"
-    image_path = "E:\\GitSpace\\RT-DETR-OpenVINO\\image\\000000570688.jpg"
-    lable_path = "E:\\GitSpace\\OpenVINO-CSharp-API\dataset\\lable\\COCO_lable.txt"
-    rtdert_infer(model_path, image_path, "GPU.0", lable_path,False)
+    if(postprocess):
+        model_path = "E:\\Model\\rtdetr_r50vd_6x_coco.onnx"
+        image_path = "E:\\GitSpace\\RT-DETR-OpenVINO\\image\\000000570688.jpg"
+        lable_path = "E:\\GitSpace\\OpenVINO-CSharp-API\dataset\\lable\\COCO_lable.txt"
+        rtdert_infer(model_path, image_path, "GPU.0", lable_path,True)
+    else:
+        model_path = "E:\\Model\\RT-DETR\\rtdetr_r50vd_6x_coco.xml"
+        image_path = "E:\\GitSpace\\RT-DETR-OpenVINO\\image\\000000570688.jpg"
+        lable_path = "E:\\GitSpace\\OpenVINO-CSharp-API\dataset\\lable\\COCO_lable.txt"
+        rtdert_infer(model_path, image_path, "GPU 0", lable_path,False)
 
 
 # The `if __name__ == '__main__':` statement is used to check whether the current script is being run
 # directly or being imported as a module.
 if __name__ == '__main__':
-    main()
+    main(False)
